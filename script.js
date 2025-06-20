9b972568019940271ff9eb8cae62e98974159cd9
Jooz-max <tlhonolofatsohlopho27@gmail.com>
2025-06-20 08:09:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,15 @@ function goTo(gender) {
   } else if (gender === 'girl') {
     window.location.href = "girl.html";
   }
-    }
+}
+
+function openSettings() {
+  const input = prompt("This button is used by the developer. Enter password:");
+  const correctPassword = "(Bonang100)"; // You can change this to your real password
+
+  if (input === correctPassword) {
+    window.location.href = "settings.html"; // Go to settings page
+  } else {
+    alert("Access denied.");
+  }
+                       }
